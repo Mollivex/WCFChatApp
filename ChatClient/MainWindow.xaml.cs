a2f0718766bbba158256e580f28301330454c838
Alex <licoms1997@gmail.com>
2023-09-16 11:52:16 +0300

    Modify MainWindow WPF form

--- ChatClient/MainWindow.xaml.cs
+++ ChatClient/MainWindow.xaml.cs
@@ -12,24 +12,33 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ChatClient.ServiceChat;
 
 namespace ChatClient
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, ServiceChat.IServiceChatCallback
     {
         bool isConnected = false;
+        ServiceChat.ServiceChatClient client;
+        int ID;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        void ConnectUser()
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+            void ConnectUser()
         {
             if (!isConnected)
             {
+                client = new ServiceChatClient(new System.ServiceModel.InstanceContext(this));
+                ID = client.Connect(tbUserName.Text);
                 tbUserName.IsEnabled = false;
                 bConDiscon.Content = "Disconnect";
                 isConnected = true;
@@ -39,6 +48,8 @@ namespace ChatClient
         {
             if (isConnected)
             {
+                client.Disconnect(ID);
+                client = null;
                 tbUserName.IsEnabled = true;
                 bConDiscon.Content = "Connect";
                 isConnected = false;
@@ -56,7 +67,29 @@ namespace ChatClient
                 ConnectUser();
             }
         }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DisconnectUser();
+        }
+
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e) { }
         private void tbMessage_TextChanged(object sender, TextChangedEventArgs e) { }
+
+        public void MsgCallback(string msg)
+        {
+            lbChat.Items.Add(msg);
+        }
+
+        private void tbMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                if(client != null)
+                {
+                    client.SendMsg(tbMessage.Text, ID);
+                    tbMessage.Text = string.Empty;
+                }
+            }
+        }
     }
 }
