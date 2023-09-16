9e045209e20523b7b4ba7df05e2fc21a27303727
Alex <licoms1997@gmail.com>
2023-09-16 07:48:24 +0300

    Implementation UI functionality (ServiceChat.cs and IServiceChat.cs)

--- wcf_chat/IServiceChat.cs
+++ wcf_chat/IServiceChat.cs
@@ -5,13 +5,27 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 
+// Description of WCFChatApp user functionality
+
 namespace wcf_chat
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServiceChat" in both code and config file together.
-    [ServiceContract]
+    [ServiceContract(CallbackContract = typeof(IServiceChatCallback))]
     public interface IServiceChat
     {
         [OperationContract]
-        void DoWork();
+        int Connect(string name);
+
+        [OperationContract]
+        void Disconnect(int id);
+
+        [OperationContract(IsOneWay = true)]
+        void SendMsg(string msg, int id);
+    }
+
+    public interface IServiceChatCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void MsgCallback(string msg);
+
     }
 }
--- wcf_chat/ServiceChat.cs
+++ wcf_chat/ServiceChat.cs
@@ -4,14 +4,72 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using System.Xml.Linq;
+
+// Implementation of WCFChatApp user functionality (UI)
 
 namespace wcf_chat
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServiceChat" in both code and config file together.
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
     public class ServiceChat : IServiceChat
     {
-        public void DoWork()
+        List<ServerUser> users = new List<ServerUser>();
+        int nextID = 1;
+
+        /// <summary>
+        /// User connection
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns> 
+        public int Connect(string name)
         {
+            ServerUser user = new ServerUser()
+            {
+                ID = nextID,
+                Name = name,
+                operationContext = OperationContext.Current
+            };
+            nextID++;
+
+            SendMsg(user.Name + " has connected to the chat!", 0);
+            users.Add(user);
+            return user.ID; 
+        }
+         
+        /// <summary>
+        /// User disconnection
+        /// </summary>
+        /// <param name="id"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Disconnect(int id)
+        {
+            var user = users.FirstOrDefault(i => i.ID == id);
+            if (user != null)
+            {
+                users.Remove(user);
+                SendMsg(user.Name + " has left the chat", 0);
+            }
+        }
+
+        /// <summary>
+        /// User send message
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="id"></param>
+        public void SendMsg(string msg, int id)
+        {
+            foreach(var item in users)
+            {
+                string answer = DateTime.Now.ToShortTimeString();
+                var user = users.FirstOrDefault(i => i.ID == id);
+                if (user != null)
+                {
+                    answer += ": " + user.Name + " ";
+                }
+                answer += msg;
+
+                item.operationContext.GetCallbackChannel<IServiceChatCallback>().MsgCallback(answer);
+            }
         }
     }
 }
