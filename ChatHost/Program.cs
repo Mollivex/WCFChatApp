ecfedbfb3a7bde4a0600982d539b9f494dc8eeb3
Alex <licoms1997@gmail.com>
2023-09-16 09:48:39 +0300

    Modify ChatHost (first launch)

--- ChatHost/Program.cs
+++ ChatHost/Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
 
 namespace ChatHost
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            using (var host = new ServiceHost(typeof(wcf_chat.ServiceChat)))
+            {
+                host.Open();
+                Console.WriteLine("Host is started!");
+                Console.ReadLine();
+            }
         }
     }
 }
